7a72844a522a17162458bb2b738f8960c66cd77b
Medeiros <hsymetry@gmail.com>
2023-06-21 23:59:11 -0300

    Flutter build web for vercel

---
+++ build/app/generated/source/buildConfig/debug/br/com/erimedeiros/cats_facts/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.erimedeiros.cats_facts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.erimedeiros.cats_facts";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
